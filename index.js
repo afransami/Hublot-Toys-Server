1a27236ed675738b16531e41d547b1e4b9f15d7a
afransami <afran.sami007@gmail.com>
2023-05-20 20:26:41 +0600

    home page extra 2 section updated

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.get ('/', (req, res)=>{
     res.send('assignment-11 is running')
 });
 
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tcuzcs8.mongodb.net/?retryWrites=true&w=majority`;
 
